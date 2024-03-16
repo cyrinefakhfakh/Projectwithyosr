c494b9d8826cfdb22c151e26e79ec7cf91465208
cyrinefakhfakh <sirinefakhfakh03@gmail.com>
2024-03-16 23:49:25 +0100

    commit

--- app/src/main/java/com/example/project/login.java
+++ app/src/main/java/com/example/project/login.java
@@ -2,7 +2,11 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        TextView sign = findViewById(R.id.signup);
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the SecondActivity
+                Intent intent = new Intent(login.this, signup.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/project/signup.java
+++ app/src/main/java/com/example/project/signup.java
@@ -2,7 +2,10 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class signup extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+
+
+
+        TextView login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the SecondActivity
+                Intent intent = new Intent(signup.this, login.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
     }
 }
