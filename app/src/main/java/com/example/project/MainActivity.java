cf13da8b4a3060d32d05109c1dc3b7f55105dbf0
yosser-gh <yghrairi71@gmail.com>
2024-03-29 00:36:26 +0100

    added splash

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -14,37 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences sharedLogin = getSharedPreferences("shared_login", MODE_PRIVATE);
-        if (sharedLogin.contains("email")){
-            Intent intent = new Intent(MainActivity.this, home.class);
-            startActivity(intent);
-        }
 
-    Button login = findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            // Create an Intent to start the SecondActivity
-            Intent intent = new Intent(MainActivity.this, login.class);
-            startActivity(intent);
-        }
-    });
-
-
-        Button sign = findViewById(R.id.sign);
-        sign.setOnClickListener(new View.OnClickListener() {
+        Thread thread = new Thread(){
             @Override
-            public void onClick(View v) {
-                // Create an Intent to start the SecondActivity
-                Intent intent = new Intent(MainActivity.this, signup.class);
-                startActivity(intent);
+            public void run() {
+                try {
+                    sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(MainActivity.this, login.class);
+                    startActivity(intent);
+                }
             }
-        });
+        };thread.start();
 
-
-
-}
-
-
-
+    }
 }
