6319d64c5df870272a0dd205b2e89ead39d46e6e
cyrinefakhfakh <sirinefakhfakh03@gmail.com>
2024-04-19 20:20:30 +0100

    try

--- app/src/main/java/com/example/project/MapsActivity.java
+++ app/src/main/java/com/example/project/MapsActivity.java
@@ -17,7 +17,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
+import android.location.Location;
+import android.location.LocationListener;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
