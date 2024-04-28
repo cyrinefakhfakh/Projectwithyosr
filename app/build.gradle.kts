fc0f180b493518824fce24ba99b90b1717527cd6
cyrinefakhfakh <sirinefakhfakh03@gmail.com>
2024-04-28 19:19:06 +0100

    login photo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.android.gms:play-services-maps:18.2.0")
-   implementation ("com.google.android.gms:play-services-location:21.2.0")}
+    implementation ("com.google.android.gms:play-services-location:21.2.0")}
 dependencies {
-    implementation ("androidx.appcompat:appcompat:1.6.1")
-}
+    implementation ("androidx.appcompat:appcompat:1.6.1")}
+
